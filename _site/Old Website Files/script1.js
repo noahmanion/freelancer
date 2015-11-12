6be97d06d7fbe150b50cc20d73d52f5af1c99cb8
noahmanion <noah.manion@gmail.com>
2015-11-11 21:12:08 -0600

    Swapped to Jekyll

---
+++ _site/Old Website Files/script1.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+var $b = $('button.learn-more'), $hide = $('.hide');
+ 
+$b.click(function() {
+var target = $(this).data("target");
+$hide.hide();
+$(target).show();
+})
+
+});
